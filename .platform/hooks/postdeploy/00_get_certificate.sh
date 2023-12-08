2d207eaa1dbc0696626342dd783123d5b7016d94
hoopesew <hoopesew@gmail.com>
2023-12-07 23:37:43 -0700

    bazinga

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d provomentalsocial.is404.net --nginx --agree-tos --email hoopesew@gmail.com
+sudo certbot -n -d psmh34.is404.net --nginx --agree-tos --email hoopesew@gmail.com
